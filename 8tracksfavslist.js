30078dd117b2ceb1c09ab2f1320ffe52edccac42
Alexey Derkach <aleksey.derkach@gmail.com>
2012-04-05 14:07:42 +0300

    Standalone js for gettings favorites list

---
+++ 8tracksfavslist.js
@@ -0,0 +1,20 @@
+function run_favs_getter () {
+  $('.user_about').append('<a href="#" id="showfavslist" style="cursor: pointer;">Show favorites songs list</div>');
+  $('.user_about').append('<textarea rows="20" cols="40" id="favslist" name="favslist" style="display: none;"></textarea>');
+  $('#showfavslist').click(function() {
+  if ($('#showfavslist').text() == 'Show favorites songs list') {
+      $('#showfavslist').text('Hide favorites songs list');
+      $('#favslist').show();
+      $('li.fav_track').each(function (index, item) {
+                    var songname = $(item).find('div.track_info span.a a').text() + " - " + $(item).find('div.track_info span.t a').text() + "\n";
+                    $('#favslist').val($('#favslist').val() + songname); 
+                });
+  }
+  else {
+      $('#showfavslist').text('Show favorites songs list');
+      $('#favslist').hide();
+  };
+  });
+}
+
+run_favs_getter ();
